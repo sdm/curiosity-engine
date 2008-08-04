4d8aa85c9c4fd3773744a79f0edb1df8a5054436
Shawn MacIntyre <sdm@openradical.com>
2008-08-04 09:37:08 -0700

    initial app layout setup

---
+++ _init.js
@@ -0,0 +1,17 @@
+// curiosity-engine -- Copyright 2008, Shawn MacIntyre; All rights Reserved
+
+
+// Core Libraries used
+core.db.modelBase();
+core.core.routes();
+
+
+// Application Libraries
+
+
+// Routes
+
+routes = new Routes();
+routes.setDefault('/controllers/default.jxp');
+
+
